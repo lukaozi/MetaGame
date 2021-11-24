c93af94dbe276f37322765eb541c16662844fed0
卢盛考 <lushengkao@37.com>
2021-11-24 18:16:54 +0800

    snow id

---
+++ server/parent/src/main/java/meta/idgenerator/DefaultDBId.java
@@ -0,0 +1,12 @@
+package meta.idgenerator;
+
+/**
+ * @author: AK-47
+ * @date: 2021/11/24
+ */
+public interface DefaultDBId {
+
+    int ACCOUNT_ID = 1;
+    int PLAYER_ID = 2;
+    int ITEM_ID = 3;
+}
---
+++ server/parent/src/main/java/meta/idgenerator/DefaultLocalId.java
@@ -0,0 +1,9 @@
+package meta.idgenerator;
+
+/**
+ * @author: AK-47
+ * @date: 2021/11/24
+ */
+public class DefaultLocalId {
+    int session_id = 99;
+}
